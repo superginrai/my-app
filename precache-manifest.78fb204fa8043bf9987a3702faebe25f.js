4d5863b647830aa33ad62a55f380700763009f35
Cory Booth <Rodimus2005@hotmail.com>
2020-02-18 17:46:04 -0600

    Updates

---
+++ precache-manifest.78fb204fa8043bf9987a3702faebe25f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a89db2a81b6d958c2e95004d979920c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "19cdff1005abfd8dca9a",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/my-app/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19cdff1005abfd8dca9a",
+    "url": "/my-app/static/js/main.045f5d78.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
